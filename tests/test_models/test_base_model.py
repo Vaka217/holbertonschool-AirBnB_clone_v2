43d84d49c8f4cc35d96949767101cf9e298b6d41
Maurlcio <thecoolermess@gmail.com>
2023-03-22 22:18:39 -0700

    untangling code

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from datetime import datetime
@@ -11,6 +10,7 @@ import pycodestyle
 import inspect
 import unittest
 storage_t = getenv("HBNB_TYPE_STORAGE")
+
 
 class test_Amenity(test_basemodel):
     """ """
