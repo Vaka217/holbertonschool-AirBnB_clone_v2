65a28a837fbb7b080948bbb2e6c12bd0fda78d2e
Maurlcio <thecoolermess@gmail.com>
2023-03-22 22:39:16 -0700

    tests

--- console.py
+++ console.py
@@ -91,7 +91,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, command):
         """ Method to exit the HBNB console"""
-        exit()
+        exit(0)
 
     def help_quit(self):
         """ Prints the help documentation for quit  """
